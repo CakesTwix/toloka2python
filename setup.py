cd13a18c702e23e6a5d568736825bd2f0482be71
CakesTwix <oleg.kiryazov@gmail.com>
2023-08-25 18:52:58 +0300

    setup: 0.1.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='toloka2python',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Бібліотека на пітоні для взаємодії з українським торрент-трекером Toloka',
+    author='CakesTwix',
+    license='GPL3',
+)
