f48c0f558f4e7dfcca8aca89c63bd148add45092
CakesTwix <oleg.kiryazov@gmail.com>
2023-09-09 11:52:24 +0300

    setup: include `models`, add `install_requires`

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='toloka2python',
-    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    packages = ['toloka2python', 'toloka2python/models'],
     version='0.1.0',
     description='Бібліотека на пітоні для взаємодії з українським торрент-трекером Toloka',
     author='CakesTwix',
