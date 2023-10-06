feeec15ff3c38917e2ac3c9c0e59d28a6904bb9c
CakesTwix <oleg.kiryazov@gmail.com>
2023-10-06 11:54:07 +0300

    core: Add Headers

--- toloka2python/__init__.py
+++ toloka2python/__init__.py
@@ -16,10 +16,23 @@ logging.basicConfig(level=logging.DEBUG)
 class Toloka:
     """Клас для взаємодії з торрент-трекером Toloka"""
 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+    }
+
     toloka_url = "https://toloka.to"
 
     def __init__(self, username: str, password: str, ssl="on"):
         self.session = requests.Session()
+        self.session.headers = self.headers
 
         # If not have cookie - do login
         if not os.path.exists("cookie.txt"):
