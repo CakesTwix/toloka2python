5eebf9c33c8568e21ce0376bb1ee965235672bf6
maksii <1761348+maksii@users.noreply.github.com>
2024-05-31 20:11:44 +0300

    beautifulsoup4 added to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='toloka2python',
     install_requires=[
-        'requests',
+        'requests', 'beautifulsoup4'
     ],
     packages = ['toloka2python', 'toloka2python/models'],
-    version='0.1.0',
+    version='0.1.1',
     description='Бібліотека на пітоні для взаємодії з українським торрент-трекером Toloka',
     author='CakesTwix',
     license='GPL3',
