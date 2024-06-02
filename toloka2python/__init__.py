17b42c24e48bea9a5fca20c23dc1cc4ae745900b
maksii <1761348+maksii@users.noreply.github.com>
2024-06-02 09:17:19 +0300

    toloka client improvement to handle invalid or bad cookies + base description

--- toloka2python/__init__.py
+++ toloka2python/__init__.py
@@ -1,22 +1,21 @@
-"""Початок моєї бібліотеки"""
-import json
+import requests
 import os
+import json
 import logging
 import re
 
 import requests
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 from datetime import datetime
-from toloka2python.models.torrent import TorrentElement, TorrentAccount, Torrent, TorrentFile
-from toloka2python.models.account import Account
+from toloka2python.models.torrent import TorrentElement, Torrent, TorrentFile
 from toloka2python.account import get_account_info
 
 # Set Logging
 logging.basicConfig(level=logging.DEBUG)
 
-
 class Toloka:
-    """Клас для взаємодії з торрент-трекером Toloka"""
+    """Class for interacting with the torrent tracker Toloka"""
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0',
@@ -31,36 +30,90 @@ class Toloka:
     }
 
     toloka_url = "https://toloka.to"
+    cookie_file = "cookie.txt"
+    max_login_attempts = 2  # Limit to prevent infinite login attempts
 
     def __init__(self, username: str, password: str, ssl="on", file: str = None):
+        self.username = username
+        self.password = password
+        self.ssl = ssl
+        self.file = file or self.cookie_file
         self.session = requests.Session()
         self.session.headers = self.headers
+        self.login_attempts = 0
 
-        # If not have cookie - do login
-        if not os.path.exists("cookie.txt"):
-            logging.info("No cookie file.")
+        self.login()
 
-            self.session.post(
+    def login(self):
+        """Handle login and session management."""
+        if not os.path.exists(self.file):
+            logging.info("No cookie file found. Logging in.")
+            self.perform_login()
+        else:
+            logging.info("Loading cookies from file.")
+            if not self.load_cookies():
+                logging.info("Cookie loading failed or expired, re-logging in.")
+                self.perform_login()
+
+    def perform_login(self):
+        """Perform login to the site and save cookies."""
+        self.login_attempts += 1
+        try:
+            response = self.session.post(
                 f"{self.toloka_url}/login.php",
                 {
-                    "username": username,
-                    "password": password,
+                    "username": self.username,
+                    "password": self.password,
                     "autologin": "on",
-                    "ssl": ssl,
+                    "ssl": self.ssl,
                     "login": "Вхід",
                 },
             )
+            response.raise_for_status()  # Check if the request was successful
+            if self.validate_cookies():
+                self.save_cookies()
+                self.login_attempts = 0  # Reset login attempts after successful login
+            else:
+                if self.login_attempts < self.max_login_attempts:
+                    logging.info("Initial cookie validation failed, trying again.")
+                    os.remove(self.file)
+                    self.session.cookies.clear()  # Clear session cookies before retry
+                    self.perform_login()  # Retry login
+                else:
+                    logging.error("Maximum login attempts reached, raising exception.")
+                    raise Exception("Failed to validate cookies after maximum retries.")
+        except RequestException as e:
+            logging.error(f"Failed to login: {e}")
+            raise
 
-            if file:
-                with open(file, "w", encoding="utf-8") as f:
-                    json.dump(requests.utils.dict_from_cookiejar(self.session.cookies), f)
+    def load_cookies(self):
+        """Load cookies from a local file and check if they are still valid."""
+        try:
+            with open(self.file, "r", encoding="utf-8") as f:
+                cookies = json.load(f)
+                self.session.cookies.update(requests.utils.cookiejar_from_dict(cookies))
+            return self.validate_cookies()
+        except (IOError, json.JSONDecodeError) as e:
+            logging.error(f"Error loading cookies: {e}")
+            return False
 
-        # We have cookie, no need do request
-        else:
-            logging.info("Open cookie file.")
-            with open("cookie.txt", "r", encoding="utf-8") as f:
-                cookies = requests.utils.cookiejar_from_dict(json.load(f))
-                self.session.cookies.update(cookies)
+    def validate_cookies(self):
+        """Validate the cookies by checking if a protected page can be accessed."""
+        check_url = f"{self.toloka_url}/f50"
+        response = self.session.get(check_url)
+        if "login.php?redirect=viewforum.php" in response.url:
+            logging.info("Cookies are invalid or expired.")
+            return False
+        logging.info("Cookies are valid.")
+        return True
+
+    def save_cookies(self):
+        """Save cookies to a local file."""
+        try:
+            with open(self.file, "w", encoding="utf-8") as f:
+                json.dump(requests.utils.dict_from_cookiejar(self.session.cookies), f)
+        except IOError as e:
+            logging.error(f"Failed to save cookies: {e}")
 
     def search(self, nm):
         """Пошук торрентів за запитом"""
@@ -153,7 +206,29 @@ class Toloka:
         # Remove extra whitespace, newline, and tab characters using regular expressions
         cleaned_content = re.sub(r'[\n\t]+', '', content)
         soup = BeautifulSoup(cleaned_content, "html.parser")
+        
+        description = ""
+        try:
+            #TBD baseline for description of quality and content
+            # format somehow?
+            start_text = "Відео:"
+            end_text = "MediaInfo"
 
+            # Extract entire text from HTML
+            text = soup.get_text()
+
+            # Find start and end index
+            start_idx = text.find(start_text)
+            end_idx = text.find(end_text, start_idx)
+
+            # Extract the relevant part
+            data = text[start_idx:end_idx].strip()
+
+            # Replace multiple whitespaces with a single space
+            description = ' '.join(data.split())
+        except Exception as e:
+            description = e
+        
         name = soup.find("a", class_="maintitle").text
         url = soup.find("a", class_="maintitle")["href"].replace("/","")
         forum = soup.select_one("td[class='nav'] h2:nth-of-type(2) a").text
@@ -202,7 +277,8 @@ class Toloka:
             thanks = int(thanks),
             rating = rating,
             torrent_url = torrent_url,
-            files = torrent_files
+            files = torrent_files,
+            description=description
         )
 
     def download_torrent(self, torrent_url: str):
